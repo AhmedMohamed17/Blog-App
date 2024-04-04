a392085fbd3927c6409901fc03d1a5e5d00e1a90
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-04-04 21:07:02 +0200

    working in postdetail

--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import PostAuthor from "../components/PostAuthor";
+import { Link } from "react-router-dom";
+import Thumbnail from "../images/blog22.jpg";
 const PostDetail = () => {
-  return <div>PostDetail</div>;
+  return (
+    <section>
+      <div className="container post-detail__container">
+        <div className="post-detail__header">
+          <PostAuthor />
+          <div className="post-detail__buttons">
+            <Link to={`/posts/werwer/edit`} className="btn sm primary">
+              Edit
+            </Link>
+            <Link to={`/posts/werwer/delete`} className="btn sm primary">
+              Delete
+            </Link>
+          </div>
+        </div>
+        <h1>This is the post title ! </h1>
+        <div className="post-detail__thumbnail">
+          <img src={Thumbnail} alt="" />
+        </div>
+        <p>lorem ya fnan</p>
+        <p>lorem ya ahmadan</p>
+        <p>lorem ya asnan</p>
+        <p>gowa el div</p>
+      </div>
+    </section>
+  );
 };
 
 export default PostDetail;
