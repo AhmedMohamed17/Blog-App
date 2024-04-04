407fecbbeea25824325f7530260541ca55c12fb1
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-04-04 02:43:42 +0200

    make description length !!

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -9,6 +9,10 @@ const PostItem = ({
   thumbnail,
   authorID,
 }) => {
+  const shortDescription =
+    description.length > 145 ? description.substr(0, 145) + "..." : description;
+  const postTitle = title.length > 30 ? title.substr(0, 145) + "..." : title;
+
   return (
     <article className="post">
       <div className="post__thumbnail">
@@ -16,9 +20,9 @@ const PostItem = ({
       </div>
       <div className="post__content">
         <Link to={`/posts/${postID}`}>
-          <h3>{title}</h3>
+          <h3>{postTitle}</h3>
         </Link>
-        <p>{description}</p>
+        <p>{shortDescription}</p>
         <div className="post__footer">
           <PostAuthor />
           <Link to={`/posts/categories/${category}`} className="btn category">
