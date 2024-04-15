0fcb344fc877fbdccae20b361de778c82690a208
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-04-15 04:48:03 +0200

    authors component maybe it's done :eyes:

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar1 from "../images/avatar1.jpg";
 import Avatar2 from "../images/avatar2.jpg";
 import Avatar3 from "../images/avatar3.jpg";
 import Avatar4 from "../images/avatar4.jpg";
 import Avatar5 from "../images/avatar5.jpg";
+import { Link } from "react-router-dom";
 
 const authorData = [
   { id: 1, avatar: Avatar1, name: "ahmed tawfeek", posts: 3 },
@@ -14,7 +15,31 @@ const authorData = [
 ];
 
 const Authors = () => {
-  return <section className="authors"></section>;
+  const [authors, setAuthors] = useState(authorData);
+  return (
+    <section className="authors">
+      {authors.length > 0 ? (
+        <div className="container authors__container">
+          {authors.map(({ id, avatar, name, posts }) => {
+            return (
+              <Link className="author" key={id} to={`posts/users${id}`}>
+                <div className="author__avatar">
+                  {" "}
+                  <img src={avatar} alt={`Image of ${name}`} />
+                </div>
+                <div className="author__info">
+                  <h4>{name}</h4>
+                  <p>{posts}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <h2 className="center">No users/authors found.</h2>
+      )}
+    </section>
+  );
 };
 
 export default Authors;
